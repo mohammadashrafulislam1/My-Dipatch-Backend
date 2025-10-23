85c4697d3959f50b06bf40d62873cf8f885651df
mohammadashrafulislam1 <ixdell41@gmail.com>
2025-10-23 01:12:57 -0500

    router

--- Router/chatRoutes.js
+++ Router/chatRoutes.js
@@ -4,8 +4,9 @@ import { upload } from "../Middleware/upload.js";
 import { verifyToken } from "../Middleware/jwt.js";
 
 export const chatRouter = express.Router();
-chatRouter.get("/:rideId", verifyToken(), getChatHistoryByRide);
-chatRouter.post("/upload", upload.single("file"), uploadChatFile);
+chatRouter.get("/customer/:rideId", verifyToken('customer'), getChatHistoryByRide);
+chatRouter.get("/:rideId", verifyToken('driver'), getChatHistoryByRide);
+chatRouter.post("/driver/upload", upload.single("file"), uploadChatFile);
 
 // Send admin message (to all, customers, drivers, or a specific user)
 chatRouter.post("/admin/send", sendAdminMessage);
