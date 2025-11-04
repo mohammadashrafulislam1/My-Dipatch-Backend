aa4e9b1400a342defadb34b808a41238580d5623
Md Ashraful Islam <ixdell41@gmail.com>
2025-11-03 19:30:22 -0600

    updated\

--- Controllers/SignupController.js
+++ Controllers/SignupController.js
@@ -169,12 +169,13 @@ export const login = async (req, res) => {
 
 
     // 6️⃣ Set the role-based cookie
-    res.cookie(cookieName, token, {
-      httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
-  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
-      maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
-    });
+   res.cookie(cookieName, token, {
+  httpOnly: true,
+  secure: process.env.NODE_ENV === "production", // only true in production
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax", // safe for localhost
+  maxAge: 7 * 24 * 60 * 60 * 1000,
+});
+
 
     // 7️⃣ Send response
     res.status(200).json({
