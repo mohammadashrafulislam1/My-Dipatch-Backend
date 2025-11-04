2a672dd39d4877b5809763b11da88e20c3a700a0
Md Ashraful Islam <ixdell41@gmail.com>
2025-11-03 19:22:17 -0600

    updated

--- Controllers/SignupController.js
+++ Controllers/SignupController.js
@@ -172,7 +172,7 @@ export const login = async (req, res) => {
     res.cookie(cookieName, token, {
       httpOnly: true,
       secure: process.env.NODE_ENV === "production",
-      sameSite: "None",
+  sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     });
 
