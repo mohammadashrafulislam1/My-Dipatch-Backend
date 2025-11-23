b643c10d6009bedcb31aa4345e3492e17fb7ba16
Md Ashraful Islam <ixdell41@gmail.com>
2025-11-23 13:47:54 -0600

    added fare rate router

--- Router/CustomerRouter/rideRoutes.js
+++ Router/CustomerRouter/rideRoutes.js
@@ -1,15 +1,18 @@
 import express from "express";
-import { getAllRides, getDriverRideHistory, getRideById, getRideHistory, requestRide, updateRideStatus } from "../../Controllers/CustomerController/RideController.js";
+import { calculateFare, getAllRides, getDriverRideHistory, getRideById, getRideHistory, requestRide, updateRideStatus } from "../../Controllers/CustomerController/RideController.js";
 import { getDriverEarnings } from "../../Controllers/RiderController/DriverController.js";
 
 export const rideRouter = express.Router();
 
 rideRouter.post("/request", requestRide);
+rideRouter.post("/calculate-fare", calculateFare);
+
 rideRouter.get("/:rideId", getRideById);
 rideRouter.get("/", getAllRides);
 rideRouter.get("/customer/:customerId/history", getRideHistory);
 
 rideRouter.put("/status/:rideId", updateRideStatus);
+
 // Add this line to your existing router
 rideRouter.get("/driver/:driverId/history", getDriverRideHistory);
 
