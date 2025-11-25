7ae87ed28926fdc51d19ba4242a261acfd50da31
Md Ashraful Islam <ixdell41@gmail.com>
2025-11-25 12:44:20 -0600

    fixed 2

--- Router/supportRoutes.js
+++ Router/supportRoutes.js
@@ -1,6 +1,6 @@
 // routes/supportRoutes.js
 import express from 'express';
-import { createTicket, getSupportCenter } from '../Controllers/SupportController.js';
+import { createTicket, getSupportAll, getSupportCenter } from '../Controllers/SupportController.js';
 import { verifyToken } from '../Middleware/jwt.js';
 
 export const supportRouter = express.Router();
@@ -8,6 +8,6 @@ export const supportRouter = express.Router();
 // Driver/Customer Support Center
 supportRouter.get('/driver', verifyToken("driver"), getSupportCenter);
 supportRouter.get('/customer', verifyToken("customer"), getSupportCenter);
-supportRouter.get('/admin', verifyToken("admin"), getSupportCenter);
+supportRouter.get('/admin', verifyToken("admin"), getSupportAll);
 supportRouter.post('/ticket/driver', verifyToken("driver"), createTicket);
 supportRouter.post('/ticket/customer', verifyToken("customer"), createTicket);
