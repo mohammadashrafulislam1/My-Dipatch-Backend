64648c9c798cb64737663c89b67d02fcb6c297e5
Md Ashraful Islam <ixdell41@gmail.com>
2025-11-25 21:07:23 -0600

    :added verify token and adming

--- Router/policyRoutes.js
+++ Router/policyRoutes.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { getPolicies, getPolicyHistory, updatePolicy } from "../Controllers/PolicyController.js";
+import { verifyToken } from "../Middleware/jwt.js";
 
 export const policyRouter = express.Router();
 
 // Get both policies (public access)
-policyRouter.get("/", getPolicies);
+policyRouter.get("/", verifyToken("admin"), getPolicies);
 
 // Update a policy (admin only)
-policyRouter.put("/:type", updatePolicy);
+policyRouter.put("/:type", verifyToken("admin"), updatePolicy);
 
 // Get policy history (admin only)
-policyRouter.get("/history", getPolicyHistory);
+policyRouter.get("/history", verifyToken("admin"), getPolicyHistory);
