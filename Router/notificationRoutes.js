543a74ac28350a735fecc3ab61dba2b0300e76a5
Md Ashraful Islam <ixdell41@gmail.com>
2025-12-06 19:14:43 -0600

    notification adde api and socket fixed 3

--- Router/notificationRoutes.js
+++ Router/notificationRoutes.js
@@ -1,13 +1,13 @@
 // routes/notificationRoutes.js
 import express from 'express';
 import { deleteNotification, getUserNotifications, markAllAsRead, markAsRead } from '../Controllers/NotificationController.js';
-import { verifyToken } from '../Middleware/jwt.js';
+import { verifyToken() } from '../Middleware/jwt.js';
 
 const notificationRouter = express.Router();
 
-notificationRouter.get('/', verifyToken, getUserNotifications);
-notificationRouter.put('/:notificationId/read', verifyToken, markAsRead);
-notificationRouter.put('/read-all', verifyToken, markAllAsRead);
-notificationRouter.delete('/:notificationId', verifyToken, deleteNotification);
+notificationRouter.get('/', verifyToken(), getUserNotifications);
+notificationRouter.put('/:notificationId/read', verifyToken(), markAsRead);
+notificationRouter.put('/read-all', verifyToken(), markAllAsRead);
+notificationRouter.delete('/:notificationId', verifyToken(), deleteNotification);
 
 export default notificationRouter;
