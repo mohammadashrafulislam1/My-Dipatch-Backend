3c58a0212cfc823e824446ce29e6b12e22632341
Md Ashraful Islam <ixdell41@gmail.com>
2025-12-06 22:38:25 -0600

    check review

--- Router/reviewRoutes.js
+++ Router/reviewRoutes.js
@@ -1,12 +1,13 @@
 import express from "express";
 import { checkReviewExists, getDriverReviews, submitReview } from "../Controllers/ReviewController.js";
+import { verifyToken } from "../Middleware/jwt.js";
 
 export const reviewRouter = express.Router();
 
 // Customer submits review
-reviewRouter.post("/", submitReview);
+reviewRouter.post("/", verifyToken, submitReview);
 
 // Driver gets their reviews
-reviewRouter.get("/driver/:driverId", getDriverReviews);
+reviewRouter.get("/driver/:driverId", verifyToken, getDriverReviews);
 
-reviewRouter.get("/check/:rideId", checkReviewExists);
+reviewRouter.get("/check/:rideId", verifyToken, checkReviewExists);
