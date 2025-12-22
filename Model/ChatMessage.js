2cf4d086e7d55120a4ca000287ca2117838beefe
Md Ashraful Islam <ixdell41@gmail.com>
2025-12-21 18:23:18 -0600

    ride id false

--- Model/ChatMessage.js
+++ Model/ChatMessage.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 
 const chatMessageSchema = new mongoose.Schema(
     {
-      rideId: { type: mongoose.Schema.Types.ObjectId, ref: "RideModel", required: true },
+      rideId: { type: mongoose.Schema.Types.ObjectId, ref: "RideModel", required: false },
       senderId: { type: mongoose.Schema.Types.ObjectId, required: true },
       senderRole: { type: String, enum: ["customer", "driver", "admin"], required: true },
       recipientId: { type: mongoose.Schema.Types.ObjectId, required: true },
