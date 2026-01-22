eb087dc58d1fc72454ff889860f616e7e5bf5f79
Md Ashraful Islam <ixdell41@gmail.com>
2026-01-22 13:07:51 -0600

    fiex

--- Router/RiderRouter/driverWalletRoutes.js
+++ Router/RiderRouter/driverWalletRoutes.js
@@ -5,6 +5,5 @@ import { verifyToken } from "../../Middleware/jwt.js";
 export const driverWalletRouter = express.Router();
 
 driverWalletRouter.get("/:driverId", getWalletSummary);
-driverWalletRouter.get("/:driverId", getWalletSummary);
 driverWalletRouter.post("/request-withdrawal", verifyToken(), requestWithdrawal);
 
