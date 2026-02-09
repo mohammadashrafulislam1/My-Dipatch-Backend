852c98f8bd5b83c86ff0d18b9ffafa532c2a5bb8
Md Ashraful Islam <ixdell41@gmail.com>
2026-02-08 22:42:51 -0600

    fixed i think

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,15 +1,16 @@
 import nodemailer from "nodemailer";
 
+console.log("process.env.SMTP_USER", process.env.SMTP_USER)
+console.log("process.env.SMTP_PASS", process.env.SMTP_PASS)
 const transporter = nodemailer.createTransport({
-  host: process.env.SMTP_HOST, // e.g. smtp.gmail.com
-  port: 587,
-  secure: false,
+  host: "smtp.gmail.com",
+  port: 465,
+  secure: true, // MUST be true for 465
   auth: {
     user: process.env.SMTP_USER,
-    pass: process.env.SMTP_PASS,
+    pass: process.env.SMTP_PASS, // App Password, NOT your real password
   },
 });
-
 export const sendEmail = async ({ to, subject, html }) => {
   try {
     await transporter.sendMail({
